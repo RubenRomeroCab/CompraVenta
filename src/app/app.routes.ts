d75744f869c1c0747ef610fb50d81ee3a60c6c36
RubenRomeroCab <rubenromerodev@gmail.com>
2024-07-27 21:45:51 +0200

    Maquetación de la tienda
    Maquetación de la tienda  y flujo del usuario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { PaleDetailsComponent } from './components/pale-details/pale-details.component';
+import { BodyComponent } from './components/body/body.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: '/body', pathMatch: 'full' },
+    {path:'home',component:HomeComponent},
+    {path:'body',component:BodyComponent},
+    {path:'pale-details',component:PaleDetailsComponent},
+    {path:'about',component:AboutComponent}
+];
---
+++ src/app/components/pale-details/pale-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pale-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './pale-details.component.html',
+  styleUrl: './pale-details.component.css'
+})
+export class PaleDetailsComponent {
+
+}
