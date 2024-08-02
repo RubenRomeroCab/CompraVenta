9afd5c152154573f12dc342757a84d686405c8e5
RubenRomeroCab <rubenromerodev@gmail.com>
2024-08-02 06:13:12 +0200

    Ajustes y limpieza del codigo
    formulario implemtado

--- src/app/components/carrito-details/carrito-details.component.ts
+++ src/app/components/carrito-details/carrito-details.component.ts
@@ -1,58 +1,80 @@
 import { Component, OnInit } from '@angular/core';
-import {   Route, Router, RouterLink } from '@angular/router';
+import { Route, Router, RouterLink } from '@angular/router';
 import { PaleService } from '../../services/pale.service';
 import { Pale } from '../../models/pale.model';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-carrito-details',
   standalone: true,
-  imports: [RouterLink,CommonModule],
+  imports: [RouterLink, CommonModule,ReactiveFormsModule ],
   templateUrl: './carrito-details.component.html',
   styleUrl: './carrito-details.component.css'
 })
-export class CarritoDetailsComponent implements OnInit{
+export class CarritoDetailsComponent implements OnInit {
 
-  pale !:Pale []
-  precioFinal:number =0;
-  ivaTotal!:number
+
+  pale !: Pale[]
+  precioFinal: number = 0;
+  ivaTotal!: number
   cantidadIva: number = 0;
-  constructor(private carritoService:PaleService,
-              private route:Router
-              
-  ){}
+checkoutForm: any;
+  constructor(private carritoService: PaleService,
+    private route: Router,
+    private fb: FormBuilder,
+
+  ) { }
 
 
   ngOnInit(): void {
     this.pale = this.carritoService.mostrarcarrtito();
     console.log(this.pale)
     this.precioFinalCompra()
+
+  
+      this.checkoutForm = this.fb.group({
+        nombre: ['', Validators.required],
+        apellidos: ['', Validators.required],
+        empresa: [''],
+        nif: ['', Validators.required],
+        pais: ['', Validators.required],
+        direccion: ['', Validators.required],
+        codigoPostal: ['', Validators.required],
+        poblacion: ['', Validators.required],
+        provincia: ['', Validators.required],
+        notas: ['']
+      });
   }
 
 
 
 
-  borrarPale(event: Event , idPale:number) {
+  borrarPale(event: Event, idPale: number) {
     event.stopPropagation();
-    const index = this.pale.findIndex(pale => pale.id ===idPale )
+    const index = this.pale.findIndex(pale => pale.id === idPale)
     if (index !== -1) {
       // Eliminar el objeto usando splice
       this.pale.splice(index, 1);
     }
-    this.precioFinalCompra()
-    this.carritoService.eliminarPale(idPale)
-    
 
-}
-  verPale(id:number){
-   this.route.navigate(['/pale-details',id])
+
+   /*  PREGUNTAR A ALFON MAÑANA SOBRE LA BASURA ESTA
+    this.carritoService.eliminarPale(idPale); */
+    this.precioFinalCompra();
+
+
+  }
+  verPale(id: number) {
+    this.route.navigate(['/pale-details', id])
     console.log('pale seleccionado')
   }
 
   precioFinalCompra() {
     // Resetear el precioFinal a cero antes de calcularlo de nuevo
     this.precioFinal = 0;
-    
+
     for (let i = 0; i < this.pale.length; i++) {
       // Asegúrate de que pale[i].precio es un número
       this.precioFinal += Number(this.pale[i].precio) || 0;
@@ -61,12 +83,25 @@ export class CarritoDetailsComponent implements OnInit{
 
   calcularTotalConIVA(): number {
     const iva = 0.21;
-   this.cantidadIva = this.precioFinal;
+    this.cantidadIva = this.precioFinal;
     return this.precioFinal + (this.precioFinal * iva);
   }
 
 
   calcularDiferencia(): number {
     return this.cantidadIva - this.precioFinal; // Calcula la diferencia (IVA)
+  }
+
+  onSubmit() {
+   /*  if (this.checkoutForm.valid) {
+      this.stripeService.createToken(this.card!).subscribe(result => {
+        if (result.token) {
+          // Aquí es donde puedes enviar el token y la información del formulario a tu servidor
+          this.processPayment(result.token);
+        } else if (result.error) {
+          console.error(result.error.message);
+        }
+      });
+    } */
   }
 }
