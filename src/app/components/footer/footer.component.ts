6980722912e02f3df1d6e6c3e18dca1d0f6ce856
RubenRomeroCab <rubenromerodev@gmail.com>
2024-08-06 02:33:17 +0200

    Cambios en el footer
    Cambios en el footer y en las rutas

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit  {
+
+  ayoActual!:number;
+
+
+  ngOnInit(): void {
+   this.ayoActual = new Date().getFullYear();
+  }
+
+  
+
+  
 
 }
